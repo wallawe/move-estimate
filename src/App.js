b8d0c92956833d9be929fb7b087bed428049ec56
[cullenwallace] <[cullenawallace@gmail.com]>
2017-11-14 15:45:31 -0500

    component refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import MoveDetail from './components/move-detail';
 import CustomerDetail from './components/customer-detail';
 
@@ -11,9 +12,38 @@ class App extends Component {
     this.state = {
       showStep1: true,
       showStep2: false,
+      fromZip: '',
+      toZip: '',
+      moveSize: 'studio',
+      moveDate: moment(),
+      fullName: '',
+      email: '',
+      phoneNo: ''
     };
 
+    // this = {
+    //   update: function(),
+    //   state: {
+    //     showStep1:
+    //   }
+    // }
+
+    this.update = this.update.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.changeStep = this.changeStep.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      moveDate: date
+    });
+  }
+
+  update(e, field) {
+    this.setState({
+      [field]: e.target.value,
+    })
   }
 
   changeStep(toStep, fromStep) {
@@ -23,16 +53,35 @@ class App extends Component {
     })
   }
 
+  submitForm() {
+    console.log(this.state)
+  }
+
   render() {
 
     return (
       <div className="container">
         <h1 className="headline">Moving Cost Estimate</h1>
         {
-          this.state.showStep1 && <MoveDetail changeStep={this.changeStep} />
+          this.state.showStep1 &&
+          <MoveDetail
+            changeStep={this.changeStep}
+            fromZip={this.state.fromZip}
+            toZip={this.state.toZip}
+            moveSize={this.state.moveSize}
+            moveDate={this.state.moveDate}
+            update={this.update}
+            handleChange={this.handleChange} />
         }
         {
-          this.state.showStep2 && <CustomerDetail changeStep={this.changeStep} />
+          this.state.showStep2 &&
+          <CustomerDetail
+            changeStep={this.changeStep}
+            update={this.update}
+            submitForm={this.submitForm}
+            fullName={this.state.fullName}
+            email={this.state.email}
+            phoneNo={this.state.phoneNo} />
         }
       </div>
     );
